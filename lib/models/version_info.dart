69c24711bb887e12116b7eade1bd13beea1cd300
pellx <pell_ship@163.com>
2026-08-17 21:05:55 +0800

    docs: add v1.0.1 update log, bump version to 1.0.1+3

--- lib/models/version_info.dart
+++ lib/models/version_info.dart
@@ -1,6 +1,6 @@
 class VersionInfo {
   /// 当前 app 版本号，与 pubspec.yaml 保持一致
-  static const String currentVersion = '0.5.1';
+  static const String currentVersion = '1.0.1';
 
   final int id;
   final String versionNumber;
